96205f210581de4746bd75dcc85fa847949985ae
Carlos Estrada <caestrada@Carloss-MBP.attlocal.net>
2019-07-26 14:45:07 -0400

    setup register and login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,15 +9,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+      theme: ThemeData.dark(),
+      initialRoute: MyHomePage.id,
+      routes: {
+        MyHomePage.id: (context) => MyHomePage(),
+        Registration.id: (context) => Registration(),
+        Login.id: (context) => Login(),
+        Chat.id: (context) => Chat(),
+      },
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  static const String id = 'HOMESCREEN';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,14 +44,225 @@ class MyHomePage extends StatelessWidget {
               Text(
                 'Tensor Chat',
                 style: TextStyle(fontSize: 40.0),
-              ),
-              SizedBox(
-                height: 50.0,
               )
             ],
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          CustomButton(
+            text: 'Log In',
+            callback: () {
+              Navigator.of(context).pushNamed(Login.id);
+            },
+          ),
+          CustomButton(
+            text: 'Register',
+            callback: () {
+              Navigator.of(context).pushNamed(Registration.id);
+            },
           )
         ],
       ),
     );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback callback;
+  final String text;
+
+  const CustomButton({this.callback, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Material(
+        color: Colors.blueGrey,
+        elevation: 6.0,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: callback,
+          minWidth: 200.0,
+          height: 45.0,
+          child: Text(text),
+        ),
+      ),
+    );
+  }
+}
+
+class Registration extends StatefulWidget {
+  static const String id = 'REGISTRATION';
+
+  Registration({Key key}) : super(key: key);
+
+  _RegistrationState createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+  String email;
+  String password;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> registerUser() async {
+    FirebaseUser user = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Chat(user: user),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tensor Chat'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Hero(
+              tag: 'logo',
+              child: Container(
+                child: Image.asset('assets/images/logo.png'),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            onChanged: (value) => email = value,
+            decoration: InputDecoration(
+                hintText: 'Enter Your Email ...',
+                border: const OutlineInputBorder()),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          TextField(
+            obscureText: true,
+            autocorrect: false,
+            onChanged: (value) => password = value,
+            decoration: InputDecoration(
+                hintText: 'Enter Your Password ...',
+                border: const OutlineInputBorder()),
+          ),
+          CustomButton(
+            text: 'Register',
+            callback: () async {
+              await registerUser();
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  static const String id = 'LOGIN';
+
+  Login({Key key}) : super(key: key);
+
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String email;
+  String password;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> loginUser() async {
+    FirebaseUser user = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Chat(user: user),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tensor Chat'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Hero(
+              tag: 'logo',
+              child: Container(
+                child: Image.asset('assets/images/logo.png'),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            onChanged: (value) => email = value,
+            decoration: InputDecoration(
+                hintText: 'Enter Your Email ...',
+                border: const OutlineInputBorder()),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          TextField(
+            obscureText: true,
+            autocorrect: false,
+            onChanged: (value) => password = value,
+            decoration: InputDecoration(
+                hintText: 'Enter Your Password ...',
+                border: const OutlineInputBorder()),
+          ),
+          CustomButton(
+            text: 'Log In',
+            callback: () async {
+              await loginUser();
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Chat extends StatefulWidget {
+  static const String id = 'CHAT';
+  final FirebaseUser user;
+
+  Chat({Key key, this.user}) : super(key: key);
+
+  _ChatState createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
